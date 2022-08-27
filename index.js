649e822bbf028a639372a7014e76444a4446b1ab
anwarul1 <anwarjewel007@gmail.com>
2022-08-28 05:39:37 +0600

    node and how its modules work

---
+++ index.js
@@ -0,0 +1,34 @@
+// node local module
+const { name, add } = require('./other.js');
+
+/* 
+// console.log(name);
+
+const result = add(2, 3);
+// console.log(result);
+
+// setTimeout(() => {
+//   console.log('hello world');
+// }, 1000);
+
+// node core module
+const http = require('http');
+
+const server = http.createServer(function (req, res) {
+  //write code here
+  console.log(`server is running...`);
+});
+
+// server.listen(5000);
+
+// third party module --> underscore
+const _ = require('underscore');
+
+const stooges = [
+  { name: 'moe', age: 40 },
+  { name: 'larry', age: 50 },
+  { name: 'curly', age: 60 },
+];
+const res = _.pluck(stooges, 'age');
+console.log(res);
+ */
---
+++ other.js
@@ -0,0 +1,5 @@
+const name = 'programming hero';
+
+const add = (num1, num2) => num1 + num2;
+
+module.exports = { name, add };
