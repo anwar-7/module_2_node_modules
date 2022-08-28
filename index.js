a886ffd724b70bff23381403eee43f34bb4303ae
anwarul1 <anwarjewel007@gmail.com>
2022-08-28 06:41:01 +0600

    first time making our own server using node module

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 // node local module
-const { name, add } = require('./other.js');
+// const { name, add } = require('./other.js');
+
+/* 
+// we can also require module this way
+const other = require('./other.js');
+console.log(other);
+
+const res = other.subtract(5, 2);
+console.log(res); 
+*/
 
 /* 
 // console.log(name);
@@ -32,3 +41,51 @@ const stooges = [
 const res = _.pluck(stooges, 'age');
 console.log(res);
  */
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  //   res.end(`hello node.js`);
+  //   console.log(req.url);
+  /* 
+  // for server like mobile app
+  if (req.url == '/') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ course: 'ACC' }));
+    // res.end();
+  }
+   */
+  /* 
+// for server like web app
+  if (req.url == '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<p>This is Home Page</p>');
+    res.end();
+  } else if (req.url == '/contact') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<p>This is Contact Page</p>');
+    res.end();
+  } else if (req.url == '/about-us') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<p>This is About-Us Page</p>');
+    res.end();
+  } */
+
+  if (req.url == '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>This is Home page</h1>');
+    res.end();
+  } else if (req.url == '/about-us') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>This is About Us Page</h1>');
+    res.end();
+  } else if (req.url == '/contact') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>This is Contact page</h1>');
+    res.end();
+  }
+});
+
+const PORT = 5000;
+server.listen(PORT);
+
+console.log(`server is listing at ${PORT}`);
--- other.js
+++ other.js
@@ -1,5 +1,6 @@
 const name = 'programming hero';
 
 const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
 
-module.exports = { name, add };
+module.exports = { name, add, subtract };
